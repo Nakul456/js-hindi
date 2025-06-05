883ba9a2b6c538e4a6a3e8e7146168273899f0a1
Nakul456 <siricillanakul@gmail.com>
2025-06-05 05:53:36 +0000

    variable

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,15 @@
+const accountId = 1236;
+let accountName="adjdk";
+var accountPassward="6666"
+accountCity="Jaipur"
+let acccountState
+
+// accountId = 2 // not allowed
+
+console.table([accountId , accountName , accountPassward ,acccountState]);
+
+/*
+prefer not to use var
+because of issue in block scope and functional variable 
+*/
+
