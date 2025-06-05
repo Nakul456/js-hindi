688ca016767b1229d56200dd8ed21f7e2a522622
Nakul456 <siricillanakul@gmail.com>
2025-06-05 05:54:08 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,29 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(3+5) // we are using node JS , not brower
+
+console.log(3
+    +
+    3); // code readiability should be high
+
+let name = "mitesh";
+let age =33;
+let isloggin = false
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value 
+// undefined =>
+// symbol => unique
+
+//object
+
+console.log(typeof age);
+console.log(typeof "mitesh");
+console.log(typeof null); //object
+console.log(typeof undefined); //
+
+
+
