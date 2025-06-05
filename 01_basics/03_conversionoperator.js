e1c07d7ae19b2bcfb61162108f4ed82b7ac3512a
Nakul456 <siricillanakul@gmail.com>
2025-06-05 06:51:35 +0000

    conversion

---
+++ 01_basics/03_conversionoperator.js
@@ -0,0 +1,42 @@
+// let score = null
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueNumber = Number(score);
+// console.log(typeof valueNumber);
+// console.log(valueNumber);
+
+
+// //"33" => 33
+// //"33abe"=>NaN
+// //true or false=>1/0
+// //Null=>0
+// //undefined=>NaN
+
+// // let isLoggedIn = 1
+// // let isLoggedIn = ""
+// let isLoggedIn = "hitesh"
+// let boolenIsLoggedIn = Boolean(isLoggedIn);
+// console.log(boolenIsLoggedIn);
+
+// // 1=>true / 0=>false
+// // "" =>false
+// // "hitesh" => true
+
+// let someNumber = 33;
+
+// let StringNumber = String(someNumber);
+// console.log(StringNumber);
+// console.log(typeof StringNumber);
+
+//*******************Operations*********************//
+
+
+
+
+
+
+
+
+
