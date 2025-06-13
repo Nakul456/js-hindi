a2dc3c1f0b0e18222b128e8b64bb279e443a38c2
Nakul456 <siricillanakul@gmail.com>
2025-06-13 09:03:06 +0000

    operators

--- 01_basics/03_conversionoperator.js
+++ 01_basics/03_conversionoperator.js
@@ -34,6 +34,42 @@
 
 
 
+// let value = 3;
+// let negValue = -value;
+// console.log(negValue);
+
+// // console.log(2+2);
+// // console.log(2-2);
+// // console.log(2*2);
+// // console.log(2/2);
+// // console.log(2%2);
+// let str1=5;
+// let str2=8;
+// let str3= str1+str2;
+// console.log(str3);
+
+// console.log("1" + 2); //12 if there is string first then it will treat as string
+// console.log(1 + "2"); //12
+// console.log("1" + 2 + 2); //122
+// console.log(1 + 2 + "2"); // 32  it the sting is in last then  it will do the first operation 
+
+// console.log((3+5) * (3-5));
+
+// console.log(+true);
+
+// console.log(+"");
+
+// let num1,num2,num3
+// num1 = num2 = num3 = 2+3
+
+
+
+
+
+
+
+
+
 
 
 
