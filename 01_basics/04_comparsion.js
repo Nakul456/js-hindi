8fac31757caff2d687a2a534444e7505ffd21f5c
Nakul456 <siricillanakul@gmail.com>
2025-06-13 14:45:45 +0000

    summary

--- 01_basics/04_comparsion.js
+++ 01_basics/04_comparsion.js
@@ -5,6 +5,24 @@
 // console.log(2 !=1);
 
 
+// console.log("2" > 1);
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0 );
+
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+
+//The above conversion gives us the difficulty Hence we dont use the above conversion
+
+
+console.log( "2" === 2);
+
+
+
+
 
 
 
