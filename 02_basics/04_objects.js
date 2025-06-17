66fdb1716af5047b2f3efb5c627683b069bb6564
Nakul456 <siricillanakul@gmail.com>
2025-06-17 06:08:49 +0000

    secondon-objects

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,25 @@
+// const tinderUser = new Object ()
+
+const tinderUser = {}
+tinderUser.id ="1234"
+tinderUser.name = "Sammy"
+tinderUser.isLoggedIn = false
+
+console.log(tinderUser);
+
+const regularUser = {
+    email : "hitesh@gmail.com" ,
+    fullname : {
+        userfullname : {
+            firstname: "Hitesh",
+            lastname : "choudhary"
+        }
+    }
+}
+
+// console.log(regularUser.fullname.userfullname.firstname);
+
+const obj1 = {1:"a" , 2:"b"}
+const obj2 = {3:"c" , 4:"d"}
+
+
