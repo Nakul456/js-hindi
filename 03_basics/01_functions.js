c6a971f84f5e68fdbed1d5c5c3a0219646165906
Nakul Siricilla <siricillanakul@gmail.com>
2025-06-25 12:43:25 +0000

    functions

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -28,3 +28,32 @@ function loginUserMessage(username) {
 // console.log(loginUserMessage("hitesh"));
 
 console.log(loginUserMessage());
+
+
+function calculateCartPrice(...num1) {
+   return num1
+}
+
+console.log(calculateCartPrice(100,200,5000));
+
+const user = {
+   username : "hitesh",
+   prices : "2"
+}
+
+function handleObject(anyobject) {
+   console.log(`Username is ${anyobject.username} and price is ${anyobject.prices}`);
+   
+}
+
+handleObject(user)
+
+const MynewArray = [100,400,500,700]
+
+function returnMysecondValue(getArray){
+         return getArray[2]
+}
+
+// console.log(returnMysecondValue(MynewArray));
+console.log(returnMysecondValue([200 , 400 ,800]));
+
