7acc3f6c8f170f2f7c4a42ba7e96d7cf467b3fec
Nakul Siricilla <siricillanakul@gmail.com>
2025-06-26 05:33:17 +0000

    iife

--- 03_basics/04_iife.js
+++ 03_basics/04_iife.js
@@ -11,3 +11,14 @@
     console.log(`DB CONNECTED R ${name}`);
     
 } )('hitesh')
+
+
+let val1 = 5
+let val2 = 10
+
+function addnum(num1 , num2 ){
+    let total = num1+num2
+    return total
+}
+let result1 = addnum(val1 ,val2)
+let result2 = addnum(10 , 2)
