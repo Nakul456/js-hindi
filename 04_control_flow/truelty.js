06a13afc6fe6c1505210af4e70076d4675c8ada7
Nakul Siricilla <siricillanakul@gmail.com>
2025-06-27 05:17:37 +0000

    truethy

--- 04_control_flow/truelty.js
+++ 04_control_flow/truelty.js
@@ -18,3 +18,24 @@ if (Object.keys(emptyObj).length === 0) {
     console.log("object is empty");
     
 }
+
+// Nullish Coalescing Operator (??): null , undefined
+
+let val1;
+
+// val1 = 5 ?? 10m
+
+// val1 = null ?? 1
+// val1 = undefined ?? 5
+
+val1 = null ?? 10 ??20
+
+console.log(val1);
+
+// Terniary Operator
+
+//condition ? true : false
+
+const iceTeaPrice = 100
+iceTeaPrice <= 80 ? console.log("less then 80") : console.log("greater then 80");
+;
