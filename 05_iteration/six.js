cc954b5d8d728fe938f723d5716d60707b3fe919
Nakul Siricilla <siricillanakul@gmail.com>
2025-06-28 11:38:26 +0000

    six

--- 05_iteration/six.js
+++ 05_iteration/six.js
@@ -36,9 +36,47 @@ myNums.forEach( (num) => {
 
 // console.log(newNums);
 
+const books = [
+    { title: 'Book One' , genre: 'Fiction', publish: 1980 , 
+    edition: 2004
+    },
+    { title: 'Book Two' , genre: 'Non-Fiction', publish: 1992 , 
+    edition: 2008
+    },
+    { title: 'Book Three' , genre: 'History', publish: 1999 , 
+    edition: 2007
+    },
+    { title: 'Book Four' , genre: 'Non-Fiction', publish: 1989 , 
+    edition: 2010
+    },
+    { title: 'Book Five' , genre: 'Science', publish: 2009 , 
+    edition: 2014
+    },
+    { title: 'Book Six' , genre: 'Fiction', publish: 1987 , 
+    edition: 2010
+    },
+    { title: 'Book Seven' , genre: 'History', publish: 1986 , 
+    edition: 1996
+    },
 
+    { title: 'Book Eight' , genre: 'History', publish: 2011 , 
+    edition: 2016
+    },
 
+    { title: 'Book Nine' , genre: 'Non-Fiction', publish: 1981 , 
+    edition: 1989
+    },
+];
 
+// const userBook = books.filter( (bk) => bk.genre === 'History')
+// const userBook = books.filter( (bk) => bk.publish ===1986)
+
+// const userBook = books.filter( (bk) => bk.publish >= 2000)
+const userBook = books.filter( (bk) =>{ 
+    return bk.publish >= 2000 && bk.genre === 'History'
+})
+
+console.log(userBook);
 
 
 
