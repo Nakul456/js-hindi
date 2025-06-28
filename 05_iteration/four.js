eb43cdb393e1794249724bde83c2c4664708bc19
Nakul Siricilla <siricillanakul@gmail.com>
2025-06-28 04:36:21 +0000

    forin

---
+++ 05_iteration/four.js
@@ -0,0 +1,20 @@
+ const myObject = {
+    js: 'Javascript',
+    cpp: 'c++',
+    rb: 'ruby',
+    swift : 'swift'
+ }
+
+ for (const key in myObject) {
+    
+    // console.log(`${key} shortcut is for ${myObject[key]}`);
+    
+ }
+
+
+ const programming = ["Js" , "cpp" , "ruby" , "swift"]
+
+ for (const key in programming) {
+   console.log(programming[key]);
+   
+ }
