1bc04720cda65daa6311b3a06b13119fddbb26f2
Nakul Siricilla <siricillanakul@gmail.com>
2025-06-28 05:54:15 +0000

    for each

--- 05_iteration/five.js
+++ 05_iteration/five.js
@@ -1,1 +1,46 @@
- 
+const coding = ["Js", "ruby", "Java", "Python", "cpp"]
+
+coding.forEach(function (value) {
+    console.log(value);
+
+})
+
+coding.forEach((item) => {
+    console.log(item);
+
+})
+
+function printMe(item) {
+    console.log(item);
+
+}
+coding.forEach(printMe)
+
+coding.forEach((item, index, arr) => {
+    console.log(item, index, arr);
+
+})
+
+
+const myCoding = [
+    {
+        languageName: 'javascript',
+        languageFileName: 'js'
+    },
+    {
+        languageName: 'java',
+        languageFileName: 'java'
+    },
+    {
+        languageName: 'cpp',
+        languageFileName: 'c++'
+    },
+]
+
+myCoding.forEach((item) => {
+
+    console.log(item.languageFileName);
+    console.log(item.languageName);
+
+
+}) 
